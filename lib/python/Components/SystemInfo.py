594912e93004bec1f5b1e3c91a34d3a6c436d579
persianpros <persianpros@yahoo.com>
2021-04-26 16:40:44 +0430

    Disable LCD and LED settings as we use VFD plugins for those.
    Seems no SH4 model has any LCD or LED but there may be some good settings in Display Setup which now is disabled so if you're interested you could find those one by one.
    
    For normal users both could be disabled to avoid further crashes.
    
    We also keep the things for easy merging with PLi and our main enigma2

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -178,3 +178,5 @@ SystemInfo["HasSDnomount"] = False
 SystemInfo["canBackupEMC"] = False
 SystemInfo["CanSyncMode"] = fileExists("/proc/stb/video/sync_mode_choices")
 SystemInfo["RFmodSupport"] = model == "spark7162"
+SystemInfo["LCDSupport"] = False
+SystemInfo["LEDSupport"] = False
